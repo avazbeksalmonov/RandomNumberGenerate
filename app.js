466b30a0c37d015dbef4bbab4da70f7ae2d869e3
Avazbek98 <avazbeksalmonov1998@mail.ru>
2023-09-11 16:35:45 +0500

    Tasodifiy raqam

---
+++ app.js
@@ -0,0 +1,22 @@
+const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const generate = () => {
+  const minEl = document.getElementById("min");
+  const maxEl = document.getElementById("max");
+  const min = Number(minEl.value);
+  const max = Number(maxEl.value);
+
+  if (minEl.value === "" || maxEl.value === "") {
+    alert("Iltimos Min va Max qiymatlarni kiriting");
+    return;
+  }
+  if (min > max) {
+    alert("Min Maxdan kichik bo'lishi kerak");
+  }
+  const placeholderEl = document.querySelector("#placeholder");
+  placeholderEl.textContent = getRandomNumber(min, max);
+};
+const btnEl = document.getElementById("generate");
+btnEl.addEventListener("click", generate);
